7b9a7e4eb50fe5c9d5f86e9333c089deab90ccfb
Liedson Delgado <liedsondelgado38@gmail.com>
2025-05-28 18:03:28 -0100

    Delete src directory

--- src/GenerateReport.java
+++
@@ -1,8 +0,0 @@
-//Classe para representar Generate que ira gerar ficheiros em concreto
-public class GenerateReport implements Generate{
-
-    @Override
-    public void generate(String p_filename,String p_filepath,Report p_report){//recebe o conteudo do report e concatena com a sua extensao
-        System.out.println(p_filename +p_report.getExtension() +"\n" +p_report.getContent());
-    }
-}
