23d82ccdc11d210dee53cdd6e70a43e021366215
Liedson Delgado <liedsondelgado38@gmail.com>
2025-05-30 14:47:10 -0100

    Delete src directory

--- src/Generate.java
+++
@@ -1,5 +0,0 @@
-//Classe para representar a interface de Generate que ira gerar ficheiros
-public interface Generate{
-    //Methods
-    public void generate(String p_filename,String p_filepath,String p_extension,Account p_account); //recebe nome do ficheiro relatorio e objeto relatorio(conteudo), gera e salva o ficheiro que foi geradoz
-}
