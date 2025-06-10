8c8503e81b5f33bd095f7a13bdd4983e79807f69
Liedson Delgado <liedsondelgado38@gmail.com>
2025-06-10 14:47:49 -0100

    Add files via upload

---
+++ src/GenerateCardReport.java
@@ -0,0 +1,29 @@
+//Classe para representar Generate que ira gerar relatorios de cartoes de credito(.json)
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.*;
+
+ class GenerateCardReport {
+    public void generateCreditCardReport(CreditCard p_card){
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+            String p_filename = "relatorio_cartao_";
+            String p_filepath = "E:/!UTA(Pratica)/POO/InteliJ_Projects/BankingSystem/files/relatorios/cartoes/";
+            String p_fileextension = ".json";
+
+            String newFileName = (p_filepath+p_filename+p_card.getCardId()+p_fileextension);
+            mapper.writeValue(new java.io.File(newFileName),p_card);
+
+            System.out.println("Relatorio de cartao gerado com sucesso!");
+        }catch(IOException error){
+            System.out.println("Erro: nao foi possivel gerar relatorio do cartao!");
+            error.printStackTrace(); //caso ocorra algum erro ele exibe o erro exato para depuracao
+        }
+    }
+}
---
+++ src/GenerateClientReport.java
@@ -0,0 +1,32 @@
+//Classe para representar Generate que ira gerar relatorios dos clientes do banco(.json)
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.IOException;
+
+public class GenerateClientReport{
+
+    public void generateClientReport(Client p_client){
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+            String p_filename = "relatorio_cliente_";
+            String p_filepath = "E:/!UTA(Pratica)/POO/InteliJ_Projects/BankingSystem/files/relatorios/clientes/";
+            String p_fileextension = ".json";
+
+            String newFileName = (p_filepath+p_filename+p_client.getClientId()+p_fileextension);
+            mapper.writeValue(new java.io.File(newFileName),p_client);
+
+            System.out.println("Relatorio de cliente gerado com sucesso!");
+        }catch(IOException error){
+            System.out.println("Erro: nao foi possivel criar relatorio do cliente!");
+            error.printStackTrace(); //caso ocorra algum erro ele exibe o erro exato para depuracao
+        }
+    }
+}
