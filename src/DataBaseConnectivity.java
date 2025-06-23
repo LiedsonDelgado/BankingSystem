22583d624e656026f1f7d4a8c727ebf552bced88
Liedson Delgado <liedsondelgado38@gmail.com>
2025-06-23 18:05:13 -0100

    Update DataBaseConnectivity.java

--- src/DataBaseConnectivity.java
+++ src/DataBaseConnectivity.java
@@ -1,1 +1,21 @@
-//Classe que estabelece a conexao com a base de dados do Sistemaimport java.sql.*;public class DataBaseConnectivity {    public static void main(String[] args) {        String url = "jdbc:mysql://localhost:3306/BankingSystemDB";        String user = "root";        String password = "Cisco@2020";        try(Connection conn = DriverManager.getConnection(url,user,password)){            System.out.println("Conexao com a Base de Dados estabelecida com sucesso!");            Statement stmnt = conn.createStatement();            ResultSet rslt = stmnt.executeQuery("show tables");            System.out.println(rslt);        }catch(SQLException error){            System.out.println("Erro de conexao: "+error.getMessage());        }    }}
+//Classe que estabelece a conexao com a base de dados do Sistema
+
+import java.sql.*;
+
+public class DataBaseConnectivity {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/BankingSystemDB";
+        String user = "root";
+        String password = "Cisco@2020";
+
+        try(Connection conn = DriverManager.getConnection(url,user,password)){
+            System.out.println("Conexao com a Base de Dados estabelecida com sucesso!");
+
+            Statement stmnt = conn.createStatement();
+            ResultSet rslt = stmnt.executeQuery("show tables");
+            System.out.println(rslt);
+        }catch(SQLException error){
+            System.out.println("Erro de conexao: "+error.getMessage());
+        }
+    }
+}
