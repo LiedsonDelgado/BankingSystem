dba9e1ea37b06f68779dba7a5207d27c645481dc
Liedson Delgado <liedsondelgado38@gmail.com>
2025-07-12 21:13:36 -0100

    Add files via upload

---
+++ src/model/Gerente.java
@@ -0,0 +1,6 @@
+//Classe que representa o Gerente que e o unico que pode adicionar e remover contas,cartoes e clientes, que tambem ele que gera relatorios
+package model;
+
+public class Gerente{
+
+}
