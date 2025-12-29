f82bed2c73fa661af947b07a6fdc5490770c0033
Liedson Delgado <liedsondelgado38@gmail.com>
2025-12-29 20:44:12 -0100

    Delete src directory

--- src/tests/StaffMemberTest.java
+++
@@ -1,9 +0,0 @@
-//Classe de testes unitarios da classe StaffMember
-package tests;
-import enums.*;
-import model.*;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-class StaffMemberTest{
-}
