3e613d26e4819afc870ab38304086a1adb2a6478
Liedson Delgado <liedsondelgado38@gmail.com>
2026-02-17 11:13:38 -0100

    Delete src directory

--- src/exceptions/InvalidAgeException.java
+++
@@ -1,8 +0,0 @@
-//Classe que cria uma Exception personalizada para tratar erros da relacionados a idades
-package exceptions;
-
-public class InvalidAgeException extends RuntimeException{
-    public InvalidAgeException(String message){
-        super(message);
-    }
-}
--- src/exceptions/InvalidPhoneNumberException.java
+++
@@ -1,8 +0,0 @@
-//Classe que cria uma Exception personalizada para tratar erros da classe PhoneNumber
-package exceptions;
-
-public class InvalidPhoneNumberException extends RuntimeException {
-    public InvalidPhoneNumberException(String message){
-        super(message);
-    }
-}
