d544d41d63777f7e252e2802d05f7c70f683f2d1
Liedson Delgado <liedsondelgado38@gmail.com>
2026-02-18 22:09:38 -0100

    Delete src directory

--- src/exceptions/InvalidEmailException.java
+++
@@ -1,8 +0,0 @@
-//Classe que cria uma Exception personalizada para tratar erros da classe Email
-package exceptions;
-
-public class InvalidEmailException extends RuntimeException {
-    public InvalidEmailException(String message){
-        super(message);
-    }
-}
--- src/exceptions/InvalidOperationException.java
+++
@@ -1,8 +0,0 @@
-//Classe que cria uma Exception personalizada para tratar erros da classe Account
-package exceptions;
-
-public class InvalidOperationException extends RuntimeException {
-    public InvalidOperationException(String message){
-        super(message);
-    }
-}
